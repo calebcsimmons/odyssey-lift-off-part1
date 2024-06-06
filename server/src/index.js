3c3562146672678056c21115420808351e434e3c
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-06-06 12:12:26 -0700

    Initialize Apollo Server setup with schema definitions in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,23 @@
-//TODO
+// Import the ApolloServer class from the @apollo/server package
+const { ApolloServer } = require("@apollo/server");
+
+// Import the startStandaloneServer function from the @apollo/server/standalone package
+const { startStandaloneServer } = require("@apollo/server/standalone");
+
+// Import the GraphQL schema definitions from the local schema file
+const typeDefs = require("./schema");
+
+// Define an asynchronous function to start the Apollo Server
+async function startApolloServer() {
+    const server = new ApolloServer({typeDefs}); // Creat new instance of ApolloServer with provided schema definitions
+    const { url } = await startStandaloneServer(server); // Define the url for sever and start server
+
+    // Verify server is running
+    console.log(`
+        Server is Running!
+        Query at ${url}
+        `);
+}
+
+// Call function to start the Apollo server
+startApolloServer();
