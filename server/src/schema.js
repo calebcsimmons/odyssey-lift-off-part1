0c2f6e223c59fe8d321db70fdb98cba3761010f2
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-06-05 21:46:04 -0700

    added type Query for homepage grid query

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,32 +2,37 @@ const gql = require("graphql-tag"); // Used to wrap GraphQL strings that Apollo 
 
 const typeDefs = gql`
 
-"A track is a group of Modules that teaches about a specific topic"
-type Track {
+    "Query to get tracks array for the homepage grid"
+    type Query {
+        tracksForHome: [Track!]!
+    }
 
-    "the track's ID number"
-    id: ID!
-    "the tracks title"
-    title: String!
-    "the track's main author"
-    author: Author!
-    "the track's main illustration to display in the track card or track page detail"
-    thumbnail: String
-    "the track's approximate length to complete, in minutes"
-    length: Int
-    "the number of modules this track contains"
-    modulesCount: Int 
-}
+    "A track is a group of Modules that teaches about a specific topic"
+    type Track {
 
-"Author of a complete track"
-type Author {
-    id: ID!
-    "Name of the author"
-    name: String!
-    "Photo of the author via URL"
-    photo: String
+        "the track's ID number"
+        id: ID!
+        "the tracks title"
+        title: String!
+        "the track's main author"
+        author: Author!
+        "the track's main illustration to display in the track card or track page detail"
+        thumbnail: String
+        "the track's approximate length to complete, in minutes"
+        length: Int
+        "the number of modules this track contains"
+        modulesCount: Int 
+    }
 
-}
+    "Author of a complete track"
+    type Author {
+        id: ID!
+        "Name of the author"
+        name: String!
+        "Photo of the author via URL"
+        photo: String
+
+    }
 
 `;
 module.exports = typeDefs;
