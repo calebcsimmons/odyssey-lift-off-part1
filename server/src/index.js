e62980cdb7bb03856f2d17ebb604dfd2665544f6
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-06-06 12:30:50 -0700

    Defining a mock object and functions that provide mocked data for server to return

--- server/src/index.js
+++ server/src/index.js
@@ -11,9 +11,38 @@ const { makeExecutableSchema } = require("@graphql-tools/schema");
 // Import the GraphQL schema definitions from the local schema file
 const typeDefs = require("./schema");
 
+// Defining a mock object and functions that provide mocked data for server to return
+const mocks = {
+    Query: () => ({
+      tracksForHome: () => [...new Array(6)],
+    }),
+    Track: () => ({
+      id: () => "track_01",
+      title: () => "Astro Kitty, Space Explorer",
+      author: () => {
+        return {
+          name: "Grumpy Cat",
+          photo:
+            "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+        };
+      },
+      thumbnail: () =>
+        "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+      length: () => 1210,
+      modulesCount: () => 6,
+    }),
+  };
+
 // Define an asynchronous function to start the Apollo Server
 async function startApolloServer() {
-    const server = new ApolloServer({typeDefs}); // Creat new instance of ApolloServer with provided schema definitions
+    const server = new ApolloServer({
+        schema: addMocksToSchema({
+            schema: makeExecutableSchema ({ typeDefs }),
+            mocks, 
+        }),
+    }); 
+    
+    // Creat new instance of ApolloServer with provided schema definitions
     const { url } = await startStandaloneServer(server); // Define the url for sever and start server
 
     // Verify server is running
