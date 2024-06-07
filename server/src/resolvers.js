2577e0d1d034b18599bbc5956889ac508f53605d
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-06-06 19:42:57 -0700

    Initial creation of resolvers for server

---
+++ server/src/resolvers.js
@@ -0,0 +1,19 @@
+const resolvers = {
+    Query: {
+
+        // Returns array of Tracks that will be used to populate
+        // the homepage grid of web client
+        tracksForHome: (_, __, {dataSources}) => {
+            return dataSources.trackAPI.getTracksForHome();
+        },
+    },
+
+    Track: {
+        author: ({authorId}, _, {dataSources}) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        },
+    },
+}
+
+
+module.exports = resolvers;
