f634db5ad0693012cc79df8fb426c3b3970067cf
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-06-07 20:52:01 -0700

    added track query to get single track by its ID in resolvers.js

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,15 +1,28 @@
 const resolvers = {
+
+    //? Entry point for all data-fetching operations.
     Query: {
-      // get all tracks, will be used to populate the homepage grid of our web client
+      
+      // Returns array of Tracks used to populate the homepage grid of the web client
       tracksForHome: (_, __, { dataSources }) => {
         return dataSources.trackAPI.getTracksForHome();
       },
+  
+      // Get a single track by its ID, for the track page
+      track: (_, { id }, { dataSources }) => {
+        return dataSources.trackAPI.getTrack(id);
+      },
     },
+  
+    //? Defines resolvers for fields of the Track object type.
     Track: {
+      
+      // Get the author details for a specific track using the authorId
       author: ({ authorId }, _, { dataSources }) => {
         return dataSources.trackAPI.getAuthor(authorId);
       },
     },
   };
-
-module.exports = resolvers;
+  
+  module.exports = resolvers;
+  
