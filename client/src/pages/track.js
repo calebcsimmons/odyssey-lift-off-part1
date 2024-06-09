8e8adc9a51d3a1240d5ded1f068b55ca3269be83
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-06-09 10:09:53 -0700

    Set up hook for client to GraphQL call

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -2,6 +2,7 @@ import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
 import { useParams } from "react-router-dom";
+import TrackDetail from "../components/track-detail";
 
 export const GET_TRACK = gql`
 query GetTrack($trackId: ID!) {
@@ -27,9 +28,20 @@ query GetTrack($trackId: ID!) {
   }
 `
 
+// Make call from client to GraphQL server
 const Track = () => {
     const { trackId = "" } = useParams();
-    return <Layout></Layout>;
+    const { loading, error, data } = useQuery(GET_TRACK, {
+        variables: { trackId },
+    });
+
+    return (
+        <Layout>
+            <QueryResult error={error} loading={loading} data={data}>
+                <TrackDetail track={data?.track} ></TrackDetail>
+            </QueryResult>
+        </Layout>
+    );
   };
    
   export default Track;
